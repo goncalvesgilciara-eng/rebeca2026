6a288aec05007edd0fa6a7b79254d7d86d0e8a52
goncalvesgilciara-eng <goncalvesgilciara@gmail.com>
2026-02-04 20:23:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ import plotly.express as px
 
 df = pd.read_csv('WHO_time_series.csv')
 
-fig1 = px.line(df, x='Date_reported', y= 'Cumulative_cases', color='Country', title= 'Dados de Covid-19 2020')
-fig1.update_layout(xaxis_title='Data', yaxis_title='Casos Acumulados', title_font_size= 30)
+fig1 = px.line(df, x='Date_reported',
+               y= 'Cumulative_cases',
+               color='Country',
+               title= 'Dados de Covid-19 2020')
+fig1.update_layout(xaxis_title='Data', yaxis_title='Casos Acumulados')
+fig.show()
 
 st.title("DASHCOVID: Um Dashboard sobre os Dados de COVID-19 - 2020")
 
